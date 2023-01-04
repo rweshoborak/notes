9b07c8e25574e206fc2b5771c891685a32873956
rweshoborak <rweshoborakelvin@ymail.com>
2023-01-04 14:54:25 +0300

    Updated streamlit, pip and added content in publisher

--- pages/3Ms_publisher.py
+++ pages/3Ms_publisher.py
@@ -4,4 +4,19 @@ st.set_page_config(
     page_icon=":earth_africa:"
 )
 st.write("# The Microsoft Publisher LECTURE")
-st.markdown(" ## Learning Outcomes:")
+
+with st.expander("Course overview"):
+    st.title('Learning Outcomes')
+
+    st.markdown('In a Microsoft Publisher course, students may be able to achieve the following learning outcomes:')
+
+    st.markdown('- Understand the features and capabilities of Microsoft Publisher, and be able to use the software to create professional-quality documents such as brochures, flyers, and business cards.')
+    st.markdown('- Learn how to use templates, master pages, and custom styles to create consistent and professional-looking documents.')
+    st.markdown('- Use text formatting tools and techniques to create and edit text in Publisher, including font, size, color, and alignment.')
+    st.markdown('- Import and manipulate graphics, including images, shapes, and clip art, and apply effects such as shadows, reflections, and 3D rotation.')
+    st.markdown('- Create and customize charts and diagrams, including line graphs, bar charts, and pie charts, and add data labels, legends, and gridlines.')
+    st.markdown('- Create and edit tables, including merging and splitting cells, inserting and deleting rows and columns, and applying formatting to specific cells or ranges.')
+    st.markdown('- Use mail merge and collaboration features to create and customize documents for mass mailings or to work on documents with other users.')
+    st.markdown('- Print and export documents to other formats, such as PDF, HTML, and JPG, and share them with others or upload them to the web.')
+    st.markdown('- Customize documents using a range of options, including creating custom shapes, adding custom effects and animations, and creating custom templates and styles.')
+
--- pages/ICT Trends.py
+++ pages/ICT Trends.py
@@ -5,3 +5,58 @@ st.set_page_config(
 )
 st.write("# The ICT Trends LECTURE")
 st.markdown(" ## Learning Outcomes:")
+st.markdown("# Current Trends in Ict")
+with st.expander("Useful ICT Tools and Programs for Business Management"):
+    st.title("Useful ICT Tools and Programs for Business Management")
+    st.markdown('There are many ICT tools and programs that can help businesses manage their operations more efficiently and effectively. Here are a few examples:')
+
+    st.markdown('- Office productivity software: Microsoft Office, Google Workspace, and other office productivity suites provide tools for creating and editing documents, spreadsheets, and presentations. These tools are essential for managing and organizing business documents and data.')
+    st.markdown('- Project management software: Asana, Trello, and other project management tools allow businesses to track and manage tasks, deadlines, and resources. These tools can help businesses stay organized and on track with their projects.')
+    st.markdown('- Customer relationship management (CRM) software: Salesforce, HubSpot, and other CRM tools allow businesses to track and manage customer interactions and data. These tools can help businesses build and maintain relationships with their customers and analyze customer data to make better business decisions.')
+    st.markdown('- Human resources management software: ADP, Workday, and other HR management tools allow businesses to manage employee data and processes such as payroll, benefits, and performance reviews. These tools can help businesses streamline HR tasks and improve employee engagement.')
+
+    st.markdown('These are just a few examples of the many ICT tools and programs available for business management. By leveraging these tools, businesses can improve their productivity and efficiency, and gain a competitive edge in their industry.')
+
+
+with st.expander("Evolution of ICT"):
+    st.title('Evolution of ICT')
+
+    st.markdown('The field of ICT has undergone significant changes and developments over the years, as new technologies have emerged and been adopted by businesses and individuals around the world. Here is a brief overview of the evolution of ICT:')
+
+    st.markdown('- 1950s: The first computers were developed, which were large and expensive machines used primarily by government agencies and research institutions. These early computers used punched cards to input data and were slow and unreliable compared to modern computers.')
+    st.markdown('- 1960s: The first computer networks were developed, which allowed computers to communicate with each other and share data. This marked the beginning of the Internet as we know it today.')
+    st.markdown('- 1970s: The first personal computers (PCs) were introduced, which were smaller and more affordable than the mainframe computers of the time. This marked the beginning of the PC revolution, which made computers more accessible to the general public.')
+    st.markdown('- 1980s: The first mobile phones were introduced, which allowed people to make phone calls from anywhere. This marked the beginning of the mobile communication revolution, which has continued to evolve with the development of smartphones and other mobile devices.')
+    st.markdown('- 1990s: The World Wide Web was invented, which revolutionized the way people access and share information online. This marked the beginning of the dot-com era, which saw the rise of e-commerce and other online businesses.')
+    st.markdown('- 2000s: The proliferation of broadband Internet and the development of social media platforms such as Facebook and Twitter revolutionized the way people communicate and share information online. This also marked the beginning of the rise of mobile devices and mobile apps, which have become an integral part of modern life.')
+    st.markdown('- 2010s: The development of cloud computing and the Internet of Things (IoT) has led to the emergence of new business models and technologies, such as on-demand services and connected devices. This has also led to the rise of big data and data analytics, which have become important tools for businesses to gain insights and make better decisions.')
+    st.markdown('- 2020s: The COVID-19 pandemic has accelerated the adoption of digital technologies, such as remote work and online education, leading to a greater reliance on ICT for daily life and business operations. The continued development of AI and machine learning has also led to the emergence of new technologies and applications in areas such as healthcare, finance, and transportation.')
+
+    st.markdown('As you can see, the evolution of ICT has been rapid and transformative, and it is likely to continue to evolve and shape the way we live and work in the future.')
+
+with st.expander("ICT influencing Business "):
+    st.title("How ICT evolution has influenced business undertaking")
+
+
+    st.markdown('The evolution of ICT has had a significant impact on how businesses operate and compete in the market. Here are a few ways that ICT has influenced business undertakings:')
+
+    st.markdown('- Increased productivity: ICT tools and programs such as office productivity software, project management software, and CRM software have allowed businesses to automate and streamline many tasks, freeing up time and resources for other activities. This has increased the productivity of businesses and allowed them to do more with less.')
+    st.markdown('- Improved communication and collaboration: The development of email, videoconferencing, and other communication technologies has made it easier for businesses to communicate and collaborate with their employees, customers, and partners. This has allowed businesses to operate more efficiently and effectively, regardless of location.')
+    st.markdown('- Enhanced customer experience: The use of ICT tools and programs such as e-commerce platforms, social media, and mobile apps has made it easier for businesses to reach and engage with their customers. This has improved the customer experience and allowed businesses to build stronger relationships with their customers.')
+    st.markdown('- Greater access to information: The proliferation of the Internet and the development of search engines and data analytics tools have made it easier for businesses to access and analyze information. This has helped businesses make better decisions and gain a competitive advantage.')
+
+    st.markdown('Overall, the evolution of ICT has had a profound impact on business undertakings, and it is likely to continue to shape the way businesses operate in the future.')
+with st.expander("Current Trends in ICT"):
+    st.title('Current Trends in ICT for Operating and Managing Businesses')
+
+    st.markdown('There are many current trends in ICT that are impacting the way businesses operate and manage their operations. Here are a few examples:')
+
+    st.markdown('- Cloud computing: The use of cloud-based services and platforms has become increasingly popular in recent years, as businesses look to reduce their IT infrastructure costs and improve their agility. Cloud computing allows businesses to access and use computing resources on-demand, without the need to invest in and maintain their own hardware and software.')
+    st.markdown('- Artificial intelligence (AI) and machine learning: The development of AI and machine learning technologies has allowed businesses to automate and optimize many tasks and processes. This has improved efficiency and allowed businesses to make better decisions based on data and analytics.')
+    st.markdown('- Big data and data analytics: The proliferation of data and the development of advanced analytics tools have made it possible for businesses to gain insights and make better decisions based on data. This has become increasingly important as businesses look to optimize their operations and gain a competitive advantage.')
+    st.markdown('- Internet of Things (IoT): The development of connected devices and sensors has allowed businesses to gather and analyze data from various sources, such as machinery, vehicles, and consumer devices. This has enabled businesses to optimize their operations and create new products and services.')
+
+    st.markdown('These are just a few examples of the many current trends in ICT that are impacting the way businesses operate and manage their operations. By staying up-to-date with these trends, businesses can position themselves to take advantage of new technologies and opportunities.')
+with st.expander("Possible questions "):
+    st.title("Review Questions")
+
